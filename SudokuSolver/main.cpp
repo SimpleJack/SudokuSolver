97db9344c464ea77c95f5e49c852187ac5fc5e35
SimpleJack <aykut@Aykuts-iMac.local>
2013-10-17 01:14:37 +0200

    Called „Feld“ now „wuerfel“ Built nonfunctional GUI of pipes Added missing include library

--- SudokuSolver/main.cpp
+++ SudokuSolver/main.cpp
@@ -8,13 +8,17 @@
 //
 
 #include <iostream>
+#include <stdio.h>
+
 
 void wait() 
 { 
-       // Setzte Puffergröße auf 0 (=> leere Puffer) 
-    setvbuf(stdin, NULL, _IONBF, 0); 
+    // Setzte Puffergröße auf 0 (=> leere Puffer)
+    setvbuf(stdin, NULL, _IONBF, 0);
+    
     // Setze Puffergröße wieder auf die ursprüngliche Größe zurück 
-    setvbuf(stdin, NULL, _IOFBF, BUFSIZ); 
+    setvbuf(stdin, NULL, _IOFBF, BUFSIZ);
+    
     // Füge alle eingelesenen Zeichen in den Puffer bis ein Enter gedrückt wird 
     // getchar() liefert dann das erste Zeichen aus dem Puffer zurück, welches wir aber ignorieren (interessiert uns ja nicht) 
     getchar();
@@ -23,34 +27,50 @@ void wait()
 
 
 
-
-
-
-
-
-
 int main(int argc, const char * argv[])
 {
-	//       x  y  z
-	int feld[9][9][9] = {0};
-	printf("Please enter the known numbers in this scheme:\n\n1A=9 5B=4 2F=3 \n\nand confirm your input with Enter.\n");
+	//          x  y  z
+	int wuerfel[9][9][9] = {0};
+        /*
+        printf("╔═══╦═══╦═══╦═══╦═══╦═══╦═══╦═══╦═══╗\n");
+        printf("║═══║═══║═══║═══║═══║═══║═══║═══║═══║\n");
+        printf("╠═══╬═══╬═══╬═══╬═══╬═══╬═══╬═══╬═══╣\n");
+        printf("║   ║   ║   ║   ║   ║   ║   ║   ║   ║\n");
+        printf("╠═══╬═══╬═══╬═══╬═══╬═══╬═══╬═══╬═══╣\n");
+        printf("║   ║   ║   ║   ║   ║   ║   ║   ║   ║\n");
+        printf("╠═══╬═══╬═══╬═══╬═══╬═══╬═══╬═══╬═══╣\n");
+        printf("║   ║   ║   ║   ║   ║   ║   ║   ║   ║\n");
+        printf("╠═══╬═══╬═══╬═══╬═══╬═══╬═══╬═══╬═══╣\n");
+        printf("║   ║   ║   ║   ║   ║   ║   ║   ║   ║\n");
+        printf("╠═══╬═══╬═══╬═══╬═══╬═══╬═══╬═══╬═══╣\n");
+        printf("║   ║   ║   ║   ║   ║   ║   ║   ║   ║\n");
+        printf("╠═══╬═══╬═══╬═══╬═══╬═══╬═══╬═══╬═══╣\n");
+        printf("║   ║   ║   ║   ║   ║   ║   ║   ║   ║\n");
+        printf("╠═══╬═══╬═══╬═══╬═══╬═══╬═══╬═══╬═══╣\n");
+        printf("║   ║   ║   ║   ║   ║   ║   ║   ║   ║\n");
+        printf("╠═══╬═══╬═══╬═══╬═══╬═══╬═══╬═══╬═══╣\n");
+        printf("║   ║   ║   ║   ║   ║   ║   ║   ║   ║\n");
+        printf("╚═══╩═══╩═══╩═══╩═══╩═══╩═══╩═══╩═══╝\n");
+       //NOT WORKING BECAUSE SPACES OR NUMBER IN TERMINAL DON'T HAVE SAME SIZE AS PIPES
+       */
+    
+    printf("Please enter the known numbers in this scheme:\n\n1A=9 5B=4 2F=3 \n\nand confirm your input with Enter.\n");
 
 
 
-
-
-	
+    
+    
 	/*
-	std::cout << feld[1][2][3] << "\n";
+     
+	std::cout << wuerfel[1][2][3] << "\n";
 	
 	Eine Testausgabe um zu sehen ob das Array auf 0 initalisiert wurde
 
 	*/
 
 
+    
 	
-
-
 	wait();
 
     return 0;
