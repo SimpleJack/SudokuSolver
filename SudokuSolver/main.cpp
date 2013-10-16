46c1dbfdaa3fe8218a2faf3d51f31b962d77e858
Dix478 <dix@goerlichs.de>
2013-10-16 22:53:37 +0200

    wait() funktion

--- SudokuSolver/main.cpp
+++ SudokuSolver/main.cpp
@@ -9,18 +9,49 @@
 
 #include <iostream>
 
+void wait() 
+{ 
+       // Setzte Puffergröße auf 0 (=> leere Puffer) 
+    setvbuf(stdin, NULL, _IONBF, 0); 
+    // Setze Puffergröße wieder auf die ursprüngliche Größe zurück 
+    setvbuf(stdin, NULL, _IOFBF, BUFSIZ); 
+    // Füge alle eingelesenen Zeichen in den Puffer bis ein Enter gedrückt wird 
+    // getchar() liefert dann das erste Zeichen aus dem Puffer zurück, welches wir aber ignorieren (interessiert uns ja nicht) 
+    getchar();
+
+} 
+
+
+
+
+
+
+
+
+
 int main(int argc, const char * argv[])
 {
-	//       Y  X  V
+	//       x  y  z
 	int feld[9][9][9] = {0};
+	printf("Please enter the known numbers in this scheme:\n\n1A=9 5B=4 2F=3 \n\nand confirm your input with Enter.\n");
 
 
+
+
+
+	
+	/*
 	std::cout << feld[1][2][3] << "\n";
+	
+	Eine Testausgabe um zu sehen ob das Array auf 0 initalisiert wurde
+
+	*/
 
 
+	
 
-	getchar();
 
+	wait();
 
     return 0;
 	
